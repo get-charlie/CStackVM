2e7f1429bee0713711dccce544ff232320044449
get-charlie <carlos.serr.dev@gmail.com>
2024-11-24 22:47:22 +0100

    Add factorial.asm as an example of recurssion

---
+++ examples/factorial.asm
@@ -0,0 +1,40 @@
+; recursive algorithm that prints the factorial of a number
+
+        dup             ; if the input is negative exit
+        neg
+        jnz     end
+        
+        dup             ; check if the input is 0
+        push    0
+        cmp
+        jnz     is0
+
+        call    fact    ; call the subroutine and print the result when it has ended
+        print
+end:    halt
+
+is0:    push    1       ; if the input was 0 print 1 and end
+        print   
+        jmp     end
+
+fact:   dup             ; calculate the next element
+        push    1
+        sub
+        
+        dup             ; if the value is 0 end recurssion
+        jnz     else
+        pop
+        ret
+
+else:   call    fact    ; otherwise call the subroutine recursively
+        mul             ; multiply all the elements on the stack after recurssion and return
+        ret
+    
+
+
+
+
+
+
+
+
