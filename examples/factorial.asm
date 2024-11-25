b2d34dd14002eea001f57ececb2bde243d76b923
get-charlie <carlos.serr.dev@gmail.com>
2024-11-25 12:25:40 +0100

    Update factorial.asm including the 'main:' tag as an example

--- examples/factorial.asm
+++ examples/factorial.asm
@@ -1,22 +1,10 @@
 ; recursive algorithm that prints the factorial of a number
 
-        dup             ; if the input is negative exit
-        neg
-        jnz     end
-        
-        dup             ; check if the input is 0
-        push    0
-        cmp
-        jnz     is0
-
-        call    fact    ; call the subroutine and print the result when it has ended
-        print
-end:    halt
-
 is0:    push    1       ; if the input was 0 print 1 and end
         print   
         jmp     end
 
+; factorial subroutine
 fact:   dup             ; calculate the next element
         push    1
         sub
@@ -29,6 +17,21 @@ fact:   dup             ; calculate the next element
 else:   call    fact    ; otherwise call the subroutine recursively
         mul             ; multiply all the elements on the stack after recurssion and return
         ret
+
+; start program at main
+main:   dup             ; if the input is negative exit
+        neg
+        jnz     end
+        
+        dup             ; check if the input is 0
+        push    0
+        cmp
+        jnz     is0
+
+        call    fact    ; call the subroutine and print the result when it has ended
+        print
+end:    halt
+
     
 
 
